0e737295547e9d2ba64e316e8b64c36cf077619d
한영수 <besthys@smartix.co.kr>
2022-07-28 11:32:43 +0900

    View 분리 - v2

---
+++ src/main/java/hello/servlet/web/frontcontroller/v3/ControllerV3.java
@@ -0,0 +1,10 @@
+package hello.servlet.web.frontcontroller.v3;
+
+import org.springframework.web.servlet.ModelAndView;
+
+import java.util.Map;
+
+public interface ControllerV3 {
+
+    ModelAndView process(Map<String, String>) parmaMap
+}
